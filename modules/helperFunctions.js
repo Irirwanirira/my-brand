b3627464fb7ffb6c3df3f1e829a8e6f0696a37a0
Irirwanirira <irijoseph41@gmail.com>
2024-01-30 01:13:26 +0200

    Used helper functions form the modules to accomplish work

---
+++ modules/helperFunctions.js
@@ -0,0 +1,19 @@
+export function toggleNavBar(prop){
+    prop.classList.toggle('active')
+}
+
+function getMsg(){
+    return JSON.parse(localStorage.getItem("messages"))
+}
+
+export function retrieveMsg(){
+    let messages = getMsg();
+    if(messages){
+        return messages
+    }else {
+        return []
+    }
+}
+// const retrievedMsg = retrieveMsg()
+// export default retrievedMsg
+
