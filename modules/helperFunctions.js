7413b3a0dcf9dcea20858e50407b2e15207607b7
Irirwanirira <irijoseph41@gmail.com>
2024-01-31 00:58:41 +0200

    Add reusable functions in the project

--- modules/helperFunctions.js
+++ modules/helperFunctions.js
@@ -1,17 +1,21 @@
-export function toggleNavBar(prop){
-    prop.classList.toggle('active')
+export function toggleNavBar(prop) {
+  prop.classList.toggle("active");
 }
 
-function getMsg(){
-    return JSON.parse(localStorage.getItem("messages"))
+export function saveLocally(items, string) {
+  const allMessages = JSON.stringify(items);
+  localStorage.setItem(string, allMessages);
 }
 
-export function retrieveMsg(){
-    let messages = getMsg();
-    if(messages){
-        return messages
-    }else {
-        return []
-    }
+function getMsg(string) {
+  return JSON.parse(localStorage.getItem(string));
 }
 
+export function retrieveFromStore(item) {
+  let store = getMsg(item);
+  if (store) {
+    return store;
+  } else {
+    return [];
+  }
+}
