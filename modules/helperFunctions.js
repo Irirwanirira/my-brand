e590082755436c79707ffe346b0361c7fc589635
Irirwanirira <irijoseph41@gmail.com>
2024-02-01 01:22:47 +0200

    Export logout function

--- modules/helperFunctions.js
+++ modules/helperFunctions.js
@@ -19,3 +19,23 @@ export function retrieveFromStore(item) {
     return [];
   }
 }
+
+export function logout(){
+    localStorage.removeItem("loggedInUser")
+    window.location.href = "../auth/loginpage.html"
+}
+
+// function navigateTo(sectionId){
+//     //Hide all sections
+//     const sections = document.querySelectorAll('section')
+//     sections.forEach(section => {
+//         section.classList.remove('active')
+//     })
+
+//     //show the selected section
+//     const selectedSection = document.getElementById(sectionId)
+//     if(selectedSection){
+//         selectedSection.classList.add("active")
+//     }
+// }
+
