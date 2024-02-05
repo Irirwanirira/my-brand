98165de3c6bac8040664e456b8a75120b3343ee7
Irirwanirira <irijoseph41@gmail.com>
2024-02-05 23:29:47 +0200

    Allow user to comment

--- UI/blog/blog.js
+++ UI/blog/blog.js
@@ -1,21 +1,24 @@
-import { retrieveFromStore } from "../../modules/helperFunctions.js";
+import {
+  retrieveFromStore,
+  saveLocally,
+} from "../../modules/helperFunctions.js";
 
 const navBar = document.querySelector(".navigation_side");
 
 navBar.innerHTML = `
     <div class="navbar_container">
-    <div class="logo_container">
-        <img class="logo" title="Logo" src="../../image/logo.png" alt="">
-    </div>
-    <nav class="nav_bar">
-        <div class="login_blog_button">
-            <h3>Login</h3>
-            <a href="../auth/loginpage.html"><img class="login_btn" title="login" src="../../image/Login.png" alt="login arrow">
-            </a>
+        <div class="logo_container">
+            <img class="logo" title="Logo" src="../../image/logo.png" alt="">
         </div>
-    </nav>
+        <nav class="nav_bar">
+            <div class="login_blog_button">
+                <h3>Login</h3>
+                <a href="../auth/loginpage.html"><img class="login_btn" title="login" src="../../image/Login.png" alt="login arrow">
+                </a>
+            </div>
+        </nav>
     </div>
-`
+`;
 
 const backArrow = document.querySelector(".back_button");
 const div = document.createElement("div");
@@ -23,8 +26,11 @@ div.classList.add("login_blog_button");
 const button = document.createElement("button");
 button.id = "backBtn";
 button.title = "Back";
-button.style.cssText = "background-color: transparent; border: none; cursor: pointer; outline: none; padding: 0; margin: 0;";
-button.addEventListener("click", () => { window.history.back()})
+button.style.cssText =
+  "background-color: transparent; border: none; cursor: pointer; outline: none; padding: 0; margin: 0;";
+button.addEventListener("click", () => {
+  window.history.back();
+});
 const img = document.createElement("img");
 img.title = "Back";
 img.src = "../../image/Go Back.png";
@@ -34,19 +40,19 @@ div.appendChild(button);
 backArrow.appendChild(div);
 
 export function populateBlogList() {
-    const blogContainer = document.querySelector(".blog_container");
-    const blogList = retrieveFromStore("blogs");
+  const blogContainer = document.querySelector(".blog_container");
+  const blogList = retrieveFromStore("blogs");
 
-    function readBlog(id){
-        const blog = blogList.find(blog => blog.id === id)
-        localStorage.setItem("readBlog", JSON.stringify(blog))
-        window.location.href = "./read_blog.html"
-    }
+  function readBlog(id) {
+    const blog = blogList.find((blog) => blog.id === id);
+    localStorage.setItem("readBlog", JSON.stringify(blog));
+    window.location.href = "./read_blog.html";
+  }
 
-    if (blogList.length > 0) {
-        blogList.forEach((blog) => {
-            let description = blog.description.slice(0, 40);   
-           blogContainer.innerHTML += `
+  if (blogList.length > 0) {
+    blogList.forEach((blog) => {
+      let description = blog.description.slice(0, 40);
+      blogContainer.innerHTML += `
            <div class="blog_card" id="blog_card_desktop_view">
             <img class="blog_image" src="${blog.photo}" alt=${blog.title}>
             <div class="blog_description">
@@ -62,7 +68,7 @@ export function populateBlogList() {
                             <button id="your_comments">
                                 <i class="fa fa-comment" aria-hidden="true"></i>
                             </button>
-                            <p>2</p>
+                            <p>${blog.comment.length}</p>
                         </div>
                         <div class="buttons">
                             <button id="your_like">
@@ -75,27 +81,25 @@ export function populateBlogList() {
             </div> 
             </div>
             `;
-        });
-        const readMoreBtn = document.querySelectorAll(".link_to_blog");
-        readMoreBtn.forEach(btn => {
-            btn.addEventListener("click", (event) => { 
-                const blogId = event.target.id;
-                readBlog(parseInt(blogId))
-            })
-        })
-    } else {
-        blogContainer.innerHTML = "<h3>No blogs available</h3>";
-    }
+    });
+    const readMoreBtn = document.querySelectorAll(".link_to_blog");
+    readMoreBtn.forEach((btn) => {
+      btn.addEventListener("click", (event) => {
+        const blogId = event.target.id;
+        readBlog(blogId);
+      });
+    });
+  } else {
+    blogContainer.innerHTML = "<h3>No blogs available</h3>";
+  }
 }
 
-function readBlog(){
-    const blogToReadContainer = document.querySelector("#all_blogs_view");
-    const blogToRead =  retrieveFromStore("readBlog");
-    console.log(blogToRead)
-
-    if(blogToRead){
-        const {title, photo, description, Date, likes, comments} = blogToRead
-        blogToReadContainer.innerHTML += `
+function readBlog() {
+  const blogToReadContainer = document.querySelector("#all_blogs_view");
+  const blogToRead = retrieveFromStore("readBlog");
+  if (blogToRead) {
+    const { title, photo, description, Date, likes, comment } = blogToRead;
+    blogToReadContainer.innerHTML += `
             <article>
                 <div class="read_blog_contents">
                     <h2 title="${title}" class="read_blog_name">${title}</h2>
@@ -109,7 +113,7 @@ function readBlog(){
                 <button id="your_comments">
                     <i class="fa fa-comment" aria-hidden="true"></i>
                 </button>
-                <p>2</p>
+                <p>${comment.length}</p>
                 </div>
                 <div class="buttons">
                     <button id="your_like">
@@ -120,38 +124,67 @@ function readBlog(){
             </section>
 
             <div class="comment_section">
-            <form class="form" action="">
-                <input type="text" name="" id="" placeholder="Comment">
+            <form class="form" id="comment-form" action="submit">
+                <input type="text" name="commentor-name" id="commentor-name" placeholder="Name">
+                <input type="text" name="commentor-comment" id="commentor-comment" placeholder="Comment">
+                <button type="submit">Comment</button>
             </form>
-            <div class="commentators">
-                <div class="comments">
-                    <div class="commentor_image">
-                        <img src="" alt="">
-                    </div>
-                    <div class="commentor_details">
-                        <h3>Rhys Calendar</h3>
-                        <p>Awesome details, Thanks for sharing</p>
-                    </div>
-                </div>
-                <div class="comments">
-                    <div class="commentor_image">
-                        <img src="" alt="">
-                    </div>
-                    <div class="commentor_details">
-                        <h3>Chremanche</h3>
-                        <p>Awesome details, Thanks for sharing</p>
-                    </div>
-                </div>
+            <h1>Comments</h1>
+            <div class="commentators"></div>
+            </div>
+`;
+
+    const commentatorContainer = document.querySelector(".commentators");
+    if (comment && comment.length > 0) {
+      comment.forEach((commentator) => {
+        commentatorContainer.innerHTML += `
+        <div class="comments">
+            <div class="commentor_details">
+                <h3>${commentator.commentorName}: </h3>
+                <p>${commentator.commentorComment}</p>
             </div>
         </div>
-        `
+    `;
+      });
     }
+
+    const commentForm = document.querySelector("#comment-form");
+    commentForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const commentorName = commentForm["commentor-name"].value;
+      const commentorComment = commentForm["commentor-comment"].value;
+
+      const blogArray = retrieveFromStore("blogs");
+      const blogIndex = blogArray.findIndex(
+        (blog) => blog.id === blogToRead.id
+      );
+
+      if (blogArray[blogIndex].comment) {
+        blogArray[blogIndex].comment.unshift({
+          commentorName,
+          commentorComment,
+        });
+      }
+      saveLocally(blogArray, "blogs");
+      commentForm.reset();
+      
+
+      commentatorContainer.innerHTML += `
+        <div class="comments">
+            <div class="commentor_details">
+                <h3>${commentorName}: </h3>
+                <p>${commentorComment}</p>
+            </div>
+        </div>
+    `;
+    });
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    if(window.location.href.includes("blog_list")){
-        populateBlogList()}
-    else if(window.location.href.includes("read_blog")){
-        readBlog()
-    }
+  if (window.location.href.includes("blog_list")) {
+    populateBlogList();
+  } else if (window.location.href.includes("read_blog")) {
+    readBlog();
+  }
 });
